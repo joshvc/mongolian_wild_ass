ff54087a7bf4d4f5048cd4ca1e2622600369e357
Josh Van Cleef <josh@joshvc.com>
2012-08-17 19:16:00 -0400

    display next event in the sidebar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def next_event
+    Event.next
+  end
+  helper_method :next_event
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,11 @@ class Event < ActiveRecord::Base
   attr_accessible :date, :description, :distance, :early_date, :early_price, 
     :late_price, :name, :regular_date, :regular_price, :tbd_info,
     :url, :location, :measure, :bonus
+
+  scope :upcoming, where('date > ?', Time.now)
+  scope :non_bonus, where('bonus IS NOT true')
+
+  def self.next
+    self.upcoming.non_bonus.order('date').first
+  end
 end
