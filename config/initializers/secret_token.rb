39166a63a19ea7f34603877e3456f951682068da
Josh Van Cleef <josh@joshvc.com>
2012-08-17 14:21:02 -0400

    initial appscrolls app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongolianWildAss::Application.config.secret_token = '2d963bacf96370c6f0c8b60f2973ce0f92ed1a52b248c620c21a6f936ae3d275620ee4de966092cff42707a8082928473137f28542bbacf7b05169925e1a93eb'
